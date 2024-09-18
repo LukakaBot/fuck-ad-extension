ac179471b9c2f0281b9d0343aaa99b488c675252
Lokaka <monishine_code@163.com>
2024-09-18 23:09:56 +0800

    feat(deps-dev): add unocss and antd

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Flex, Switch } from "antd";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isHideLoginModal, setIsHideLoginModal] = useState(false);
+
+  function handleChangeLoginModalState(checked: boolean) {
+    setIsHideLoginModal(checked);
+    console.log(checked);
+    console.log(isHideLoginModal);
+  }
 
   return (
     <>
       <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Flex className="w-full" vertical>
+          <Flex justify="space-between" align="center">
+            <p>CSDN 登录弹窗隐藏</p>
+            <Switch
+              defaultChecked={isHideLoginModal}
+              onChange={handleChangeLoginModalState}
+            />
+          </Flex>
+        </Flex>
       </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
     </>
   );
 }
